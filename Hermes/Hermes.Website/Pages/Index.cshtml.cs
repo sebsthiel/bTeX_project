e72f4ce06763a0225bd9d9a46f1c1719c4c3b20e
abea <abea@itu.dk>
2022-02-03 15:51:19 +0100

    Index file now saves the file as a string in c#

--- Hermes/Hermes.Website/Pages/Index.cshtml.cs
+++ Hermes/Hermes.Website/Pages/Index.cshtml.cs
@@ -1,25 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-
+using System.IO;
+using System.Threading.Tasks;
+using System;
 namespace Hermes.Website.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private IHostingEnvironment _environment;
+        public IndexModel(IHostingEnvironment environment)
         {
-            _logger = logger;
+            _environment = environment;
         }
-
-        public void OnGet()
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+        public string FileAsString { get; set; }
+        
+        public void OnPost()
         {
-
+            if (Upload.Length > 0)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    Upload.CopyTo(ms);
+                    ms.Position = 0;
+                    StreamReader sr = new StreamReader(ms);
+                    string s = sr.ReadToEnd();
+                    FileAsString = s;
+                }
+            }
+            Console.WriteLine(FileAsString);
+          
         }
     }
 }
--- Hermes/Hermes.Website/wwwroot/js/site.js
+++ Hermes/Hermes.Website/wwwroot/js/site.js
@@ -1,14 +1,14 @@
-﻿var test;
-document.getElementById("fileInput").addEventListener("change", function () {
+﻿//var test;
+//document.getElementById("fileInput").addEventListener("change", function () {
 
-    var fr = new FileReader();
-    fr.onload = function () {
-        test = fr.result
+//    var fr = new FileReader();
+//    fr.onload = function () {
+//        test = fr.result
         //document.getElementById('output').textContent = test;
-    }
+//    }
 
-    fr.readAsText(this.files[0]);
+//    fr.readAsText(this.files[0]);
 
 
-})
+//})
 
