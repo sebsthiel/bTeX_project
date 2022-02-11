4beeb303140deeb630f474e08ae5fb051d68f986
sebsthiel <sebs@Sebastians-MacBook-Air.local>
2022-02-11 14:58:03 +0100

    Rework section env

--- Hermes/Hermes.Website/wwwroot/js/site.js
+++ Hermes/Hermes.Website/wwwroot/js/site.js
@@ -11,23 +11,27 @@ document.getElementById("fileInput").addEventListener("change", function () {
 
 })
 
-function node(type, name, createdAt, references) {
-    this.type = type;
+//Label
+// ccould use this node for referencing articles
+// by adding a field "informationNode"
+// for a normal label this would just be = null
+function node(name, createdAt) {
     this.name = name;
     this.createdAt = createdAt;
-    this.references = references;
 }
 
-function env(name, text, counter, countersShouldReset, countersShouldUpdate) {
+function env(name, text, counter, countersShouldReset, countersShouldUpdate, createdAt, references) {
     this.name = name;
     this.text = text;
     this.counter = counter;
     this.countersShouldReset = countersShouldReset;
     this.countersShouldUpdate = countersShouldUpdate;
+    this.createdAt = createdAt;
+    this.references = references;
 }
 
-function add(node, ref) {
-    node.references.push(ref);
+function add(env, ref) {
+    env.references.push(ref);
 }
 
 function startsWith(str1, str2) {
@@ -46,53 +50,68 @@ function startsWith(str1, str2) {
     return true;
 }
 
-// enums
-const Label = 0;
-const Ref = 1;
-const Section = 2;
-const Cite = 3;
-const Theorem = 4;
-
 // node_name -> node dict
 var node_dict = {};
 
 
-var sectionEnv = new env("section", "section", 0, [], [])
+var sectionEnv = new env("section", "section", 0, [], []);
 // list of env made with the \newtheorem command
 var env_dict = {};
 env_dict["section"] = sectionEnv;
 
 function searchFile(fileText) {
-    const regex = /((?<type>ref|label|section|cite?(p|t|author|year)?\*?){(?<typeName>.+?)})|(?<newtheorem>newtheorem)(?<envName>{.+?})(?<arg2>{.+?}|\[.+?\])(?<arg3>{.+?}|\[.+?\])?/gm;
+    const regex = /((?<type>ref|label|begin|end|section|cite?(p|t|author|year)?\*?){(?<typeName>.+?)})|(?<newtheorem>newtheorem)(?<envName>{.+?})(?<arg2>{.+?}|\[.+?\])(?<arg3>{.+?}|\[.+?\])?/gm;
     ///\\((ref|label|section|cite?(p|t|author|year)?\*?){(.+?)})|(newtheorem)({.+?})({.+?}|\[.+?\])({.+?}|\[.+?\])?/gm;
-    let currentSection = "";
+    let createdAt = sectionEnv.name;
 
     let match = regex.exec(fileText);
     do {
-
         let node1 = null;
         if (match.groups.type == "section") {
             //TODO What if sub-sections?
             //currentSection++;
-            console.log(`Section ${match.groups.typeName} is defined in ${currentSection}`);
-            currentSection = match.groups.typeName;
-            node1 = new node(Section, match.groups.typeName, currentSection, []);
+            console.log(`Section ${match.groups.typeName} is defined in ${createdAt}`);
+            createdAt = match.groups.typeName; 
+            //node1 = new node(Section, match.groups.typeName, currentSection, []);
+
+            // currentSection = this new env //TODO: counter should not be zero
+            env_dict["section"].counter = env_dict["section"].counter + 1;
+            let newEnv = new env(match.groups.typeName, "section", env_dict["section"].counter, [], [], createdAt, []);
+            createdAt = newEnv.name;
+            env_dict[newEnv.name] = newEnv;
         }
         else if (match.groups.type == "label") {
-            console.log(`Label ${match.groups.typeName} is defined in ${currentSection}`);
-            node1 = new node(Label, match.groups.typeName, currentSection, []);
+            console.log(`Label ${match.groups.typeName} is defined in ${createdAt}`);
+            node1 = new node(match.groups.typeName, createdAt);
         }
         else if (match.groups.type == "ref") {
-            console.log(`Ref ${match.groups.typeName} is defined in ${currentSection}`);
-            node1 = new node(Ref, match.groups.typeName, currentSection, []);
+            //TODO Ref should not create new node1 but should add a reference
+            // from current env
+            // if not in env then currentSection
+            console.log(`Ref ${match.groups.typeName} is defined in ${createdAt}`);
+            //node1 = new node(Ref, match.groups.typeName, createdAt, []);
+
+            // adding reference to the currentEnv references.
+            // this reference is a name of a node (label)
+            add(env_dict[createdAt], match.groups.typeName);
         }
         else if (startsWith(match.groups.type, "cite")) {
-            console.log(`Cite ${match.groups.typeName} is defined in ${currentSection}`);
-            node1 = new node(Cite, match.groups.typeName, currentSection, []);
+            console.log(`Cite ${match.groups.typeName} is defined in ${createdAt}`);
+            //node1 = new node(Cite, match.groups.typeName, createdAt, []);
+            //add(env_dict[createdAt], match.groups.typeName);
+
+        }
+        else if (match.groups.type, "begin"){
+            console.log("Start env")
+            let tmp = match.groups.typeName;
+
+            // counter ++
+            // go through lists and update / reset
+
         }
 
         if (match.groups.newtheorem == "newtheorem") {
-            console.log(`Theorem ${match.groups.newtheorem} is defined in ${currentSection}`);
+            console.log(`Theorem ${match.groups.newtheorem} is defined in ${createdAt}`);
 
             let envName = String(match.groups.envName).replace(/{|}/gm, '');
             let group7 = String(match.groups.arg2);
@@ -123,7 +142,6 @@ function searchFile(fileText) {
             } else {
                 envText = group8.replace(/{|}/gm, '');
 
-
                 counterName = group7.replace(/\[|\]/gm, '');
                 // create env
                 // add env to env_dict
@@ -147,8 +165,9 @@ function searchFile(fileText) {
 
 
         console.log(node_dict);
-        if (currentSection in node_dict) {
-            let tmp = node_dict[currentSection];
+        // Add current node to section
+        if (createdAt in node_dict) {
+            let tmp = node_dict[createdAt];
             add(tmp, match.groups.typeName);
         }
 
