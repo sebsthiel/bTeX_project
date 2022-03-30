0e4366195b0e3117f4757d51cf76805a0145c434
sebsthiel <sebs@Sebastians-MacBook-Air.local>
2022-03-30 11:08:33 +0200

    Simple post request works with server

--- Hermes/Hermes.Website/Controllers/PdfController.cs
+++ Hermes/Hermes.Website/Controllers/PdfController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Hermes.Website.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -44,6 +45,29 @@ namespace Hermes.Website.Controllers
             
         }
 
+        [HttpPost]
+        public IActionResult Post(IFormFile file)
+        {
+           
+            //Console.WriteLine("hallo: ", data["file"]);
+            Console.WriteLine("POST REQUEST");
+            if(file == null)
+            {
+                Console.WriteLine("is NULL");
+            }
+            Console.WriteLine(file);
+            Console.WriteLine("Len: "+ file.Length);
+
+            FileStream stream = new FileStream("/Users/sebs/Code/6Semester/Bachelor/Codebase/bTeX_project/Hermes/Hermes.Website/tester/main.pdf", FileMode.Open);
+            return new FileStreamResult(stream, "application/pdf");
+
+
+        }
+
+
+
         
+
+
     }
 }
